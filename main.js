02516029a181b22c4f1a78a04cec35f5febf9ad0
E-BOM-E <e.booom.e@gmail.com>
2020-05-18 19:14:35 +0900

    JS, Home Contact Button

--- main.js
+++ main.js
@@ -4,20 +4,25 @@
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
-  if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar--dark");
-  } else {
-    navbar.classList.remove("navbar--dark");
-  }
+  if (window.scrollY > navbarHeight) navbar.classList.add("navbar--dark");
+  else navbar.classList.remove("navbar--dark");
 });
 
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (e) => {
-  const target = e.target;
-  const link = target.dataset.link;
+  const link = e.target.dataset.link;
   if (link == null) return;
-  console.log(e.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+//Handle click on "contact me" button on home
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", (e) => {
+  scrollIntoView(e.target.dataset.link);
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
